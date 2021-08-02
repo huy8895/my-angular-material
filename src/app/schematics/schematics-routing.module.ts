0f91a406cacf0817a4991435238236cd2e5db3a2
huytv <huy8895@gmail.com>
2021-08-02 22:11:30 +0700

    nested router-outlet

--- src/app/schematics/schematics-routing.module.ts
+++ src/app/schematics/schematics-routing.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {SchematicsComponent} from "./schematics.component";
+import {AddressFormComponent} from "./address-form/address-form.component";
 
 const routes: Routes = [
   {
     path: '',
-    component: SchematicsComponent
+    component: SchematicsComponent,
+    children: [
+      {
+        path: 'address-form',
+        component: AddressFormComponent
+      }
+    ]
   }
-];
+]
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
