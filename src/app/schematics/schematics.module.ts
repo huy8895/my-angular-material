25be6af69f5d80934ce5c83339ae508b223489d0
huytv <huy8895@gmail.com>
2021-08-02 21:32:28 +0700

    ng generate @angular/material:address-form schematics/address-form

--- src/app/schematics/schematics.module.ts
+++ src/app/schematics/schematics.module.ts
@@ -2,13 +2,26 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SchematicsRoutingModule } from './schematics-routing.module';
+import { AddressFormComponent } from './address-form/address-form.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCardModule } from '@angular/material/card';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [AddressFormComponent],
   imports: [
     CommonModule,
-    SchematicsRoutingModule
+    SchematicsRoutingModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatCardModule,
+    ReactiveFormsModule
   ]
 })
 export class SchematicsModule { }
