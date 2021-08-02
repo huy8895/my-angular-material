546341c200ef5410eba9db13a28ca539a739c81d
huytv <huy8895@gmail.com>
2021-08-02 22:29:13 +0700

    ng generate @angular/material:navigation schematics/navigation

--- src/app/schematics/schematics-routing.module.ts
+++ src/app/schematics/schematics-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {SchematicsComponent} from "./schematics.component";
 import {AddressFormComponent} from "./address-form/address-form.component";
+import {NavigationComponent} from "./navigation/navigation.component";
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
       {
         path: 'address-form',
         component: AddressFormComponent
+      },
+      {
+        path: 'navigation',
+        component: NavigationComponent
       }
     ]
   }
--- src/app/schematics/schematics.module.ts
+++ src/app/schematics/schematics.module.ts
@@ -10,10 +10,16 @@ import { MatRadioModule } from '@angular/material/radio';
 import { MatCardModule } from '@angular/material/card';
 import { ReactiveFormsModule } from '@angular/forms';
 import { SchematicsComponent } from './schematics.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import {NavigationComponent} from "./navigation/navigation.component";
 
 
 @NgModule({
-  declarations: [AddressFormComponent, SchematicsComponent],
+  declarations: [AddressFormComponent, SchematicsComponent, NavigationComponent],
   imports: [
     CommonModule,
     SchematicsRoutingModule,
@@ -22,7 +28,12 @@ import { SchematicsComponent } from './schematics.component';
     MatSelectModule,
     MatRadioModule,
     MatCardModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ]
 })
 export class SchematicsModule { }
