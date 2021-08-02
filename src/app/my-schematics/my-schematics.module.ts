44ddbc6d5aba4da33b1c7ab811235309ce0b2f36
huytv <huy8895@gmail.com>
2021-08-02 20:29:02 +0700

    ng g component my-schematics/address-form

--- src/app/my-schematics/my-schematics.module.ts
+++ src/app/my-schematics/my-schematics.module.ts
@@ -1,9 +1,11 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import { AddressFormComponent } from './address-form/address-form.component';
 
 
 @NgModule({
   declarations: [
+    AddressFormComponent
   ],
   imports: [
     CommonModule
