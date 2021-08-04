014959741fb452a2820926751714b31eed68b955
huytv <huy8895@gmail.com>
2021-08-04 21:40:26 +0700

    ng generate module Theming --routing

---
+++ src/app/theming/theming.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ThemingRoutingModule } from './theming-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ThemingRoutingModule
+  ]
+})
+export class ThemingModule { }
